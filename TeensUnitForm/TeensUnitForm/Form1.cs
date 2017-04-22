89aeb076e5fd89362127c10d4bb28183742b8b69
Hackerma <clement.fang@epita.fr>
2017-04-22 13:52:04 +0200

    rectif

--- TeensUnitForm/TeensUnitForm/Form1.cs
+++ TeensUnitForm/TeensUnitForm/Form1.cs
@@ -12,6 +12,43 @@ namespace TeensUnitForm
 {
     public partial class Form1 : Form
     {
+        public static bool checkBirth(string s1, string s2, string s3, short y, short m, short d)
+        {
+            if (s1 == "" || s2 == "" || s3 == "")
+                return false;
+
+            short Y = short.Parse(s1);
+            short M = short.Parse(s2);
+            short D = short.Parse(s3);
+
+            if (m == 2 && D > 27)
+                return false;
+
+            if (m > 12 || m < 0 || D < 0 || D > 31)
+                return false;
+
+            if ((m == 4 || m == 6 || m == 9 || m == 11 && D > 30))
+                return false;
+
+            if (y - 24 > Y && y - 13 < Y)
+                return false;
+
+            if (y - 24 < Y && y - 13 > Y)
+                return true;
+
+
+            if (y - 24 == Y)
+            {
+                if (m != M)
+                    return m > M;
+                return d > D;
+            }
+
+            if (m != M)
+                return m < M;
+            return d <= D;
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -28,6 +65,11 @@ namespace TeensUnitForm
         }
 
         private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
